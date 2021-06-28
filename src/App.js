15f6b3be4ffc7f38034cc5e3f79b0776c1615038
Chinthujan Nandakumar <19415864+chinthujan@users.noreply.github.com>
2021-06-28 16:09:07 -0400

    build fix

--- src/App.js
+++ src/App.js
@@ -4,27 +4,25 @@ import Skills from "./Components/Skills";
 import Work from "./Components/Work";
 import Goals from "./Components/Goals";
 import Contact from "./Components/Contact";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function MainPage(props) {
-  return (
-    <div className="App">
-      <Hero />
-      <Skills />
-      <Work />
-      <Goals />
-      <Contact />
-    </div>
-  );
+ return (
+ <div className="App">
+ <Hero />
+ <Skills />
+ <Work />
+ <Goals />
+ <Contact />
+ </div>
+ );
 }
-
 export default function App() {
-  return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component={MainPage} />
-      </Switch>
-    </Router>
-  );
+ return (
+ <Router>
+ <NavBar />
+ <Switch>
+ <Route exact path="/" component={MainPage} />
+ </Switch>
+ </Router>
+ );
 }
